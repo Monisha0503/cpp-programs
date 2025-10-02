b2d20bf7488400f768cc12b9b32e5588769f222f
Monisha0503 <monishagnanaprakasam4@gmail.com>
2025-10-02 23:36:59 +0530

    Add files via upload

---
+++ BinarySearchTree .cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Function to create new node
+Node* createNode(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+// Insert a node in BST
+Node* insert(Node* root, int value) {
+    if (root == nullptr) {
+        return createNode(value);
+    }
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+
+// Search in BST
+bool search(Node* root, int key) {
+    if (root == nullptr) return false;
+    if (root->data == key) return true;
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+// Inorder Traversal (Left -> Root -> Right)
+void inorder(Node* root) {
+    if (root != nullptr) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+// Preorder Traversal (Root -> Left -> Right)
+void preorder(Node* root) {
+    if (root != nullptr) {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Postorder Traversal (Left -> Right -> Root)
+void postorder(Node* root) {
+    if (root != nullptr) {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+
+    // Insert nodes into BST
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+    cout << "\n";
+
+    cout << "Preorder Traversal: ";
+    preorder(root);
+    cout << "\n";
+
+    cout << "Postorder Traversal: ";
+    postorder(root);
+    cout << "\n";
+
+    // Searching
+    int key = 60;
+    if (search(root, key))
+        cout << key << " found in BST\n";
+    else
+        cout << key << " not found in BST\n";
+
+    return 0;
+}
